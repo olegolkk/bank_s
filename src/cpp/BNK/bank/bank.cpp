04db66f7ecc16e1f3d382c86cec33365bd4468c3
OlegO <olegoleksuk@gmail.com>
2024-05-07 23:15:29 +0300

    Исправлена часть ошибок

--- src/cpp/BNK/bank/bank.cpp
+++ src/cpp/BNK/bank/bank.cpp
@@ -459,13 +459,15 @@ void BANK_Struct::HDFRead(hid_t parent_id, const std::string& Group)
     MPI_MODE = result_int32[1];
     this->create(GroupName, MPI_MODE, NQUEUE);
 
-    if (!( = ENV_H5::HDF_Read(KQUESE, group_id, "KQUESE")))
+    INT64 kquese_length;
+    if (!(kquese_length = ENV_H5::HDF_Read(KQUESE, group_id, "KQUESE")))
     {
         error_message[1] = "Error in bnkHDFRead. KQUESE array cannot be read.";
         StopByError(-3001, &error_message);
     }
 
-    if (!( = ENV_H5::HDF_Read(KQUES, group_id, "KQUES")))
+    INT64 kques_length;
+    if (!(kques_length = ENV_H5::HDF_Read(KQUES, group_id, "KQUES")))
     {
         error_message[1] = "Error in bnkHDFRead. KQUES array cannot be read.";
         StopByError(-3001, &error_message);
@@ -548,21 +550,23 @@ void BANK_Struct::HDFWrite(hid_t parent_id, const std::string& Group)
     }
 
     // Write KQUES array dataset.
-    if (!ENV_H5::HDF_Write(KQUES, , group_id, "KQUES"))
+    if (!ENV_H5::HDF_Write(KQUES, LHEADS, group_id, "KQUES"))
     {
         error_message[1] = "Error in bnkHDFWrite. KQUES array cannot be written.";
         StopByError(-3001, &error_message);
     }
 
     // Write BnkMemb array dataset.
-    if (!ENV_H5::HDF_Write(BnkMemb, , group_id, "BnkMemb"))
+    INT64 bnkMembLength = sizeof(BnkMemb) / sizeof(BnkMemb[0]);
+    if (!ENV_H5::HDF_Write(BnkMemb, bnkMembLength, group_id, "BnkMemb"))
     {
         error_message[1] = "Error in bnkHDFWrite. BnkMemb array cannot be written.";
         StopByError(-3001, &error_message);
     }
 
     // Write QueueWeight array dataset.
-    if (!ENV_H5::HDF_Write(QueueWeight, , group_id, "QueueWeight"))
+    INT64 QueueWeightLength = sizeof(QueueWeight) / sizeof(QueueWeight[0]);
+    if (!ENV_H5::HDF_Write(QueueWeight, QueueWeightLength, group_id, "QueueWeight"))
     {
         error_message[1] = "Error in bnkHDFWrite. QueueWeight array cannot be written.";
         StopByError(-3001, &error_message);
